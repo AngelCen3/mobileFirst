478d0d4a3f1847a9d447b145cb7f6c6ed4216daf
BetoRobert <wyjawy@hotmail.com>
2024-02-21 11:38:09 -0500

    Implementacion BEM

--- resources/views/livewire/main-page.blade.php
+++ resources/views/livewire/main-page.blade.php
@@ -1,10 +1,11 @@
 <div>
     <header>
-        <img src="" alt="">
-        <div>
+        <!--<link href="{-?!-{ asset('css/styles.css') }}" rel="stylesheet">-->
+        <img src="{{ asset('assets/img/logo.svg') }}" alt="">
+        <div class="header--title-container">
             <h1>La proxima revolucion en el intercambio de criptomonedas</h1>
             <p>Batatabit te ayuda a navegar entre los diferentes precios y tendencias</p>
-            <a href="">Conoce nuestros planes <span>i</span></a>
+            <a href="" class="header--button">Conoce nuestros planes <span>i</span></a>
         </div>
     </header>
     <main>
